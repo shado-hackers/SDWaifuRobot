753a8d43083caefe0e6f5fcb53d9d58dcfde503c
NOTHING <70184956+shado-hackers@users.noreply.github.com>
2024-09-10 05:32:05 +0530

    Update imageReverse.py

--- plugins/imageReverse.py
+++ plugins/imageReverse.py
@@ -1,8 +1,41 @@
 # Copyright 2023 Qewertyy, MIT License
 import traceback
+from httpx import AsyncClient
+import base64
+import requests
 from pyrogram import Client, filters, types as t
 from Utils import ReverseImageSearch, getFile, uploadToTelegraph, createMessage
 
+
+async def uploadToImgur(file: str):
+    try:
+        # Read the image file and encode it to base64
+        with open(file, 'rb') as f:
+            data = f.read()
+            base64_data = base64.b64encode(data)
+
+        # Set API endpoint and headers for Imgur upload
+        url = "https://api.imgur.com/3/image"
+        headers = {"Authorization": "Client-ID a10ad04550b0648"}
+
+        # Upload image to Imgur and get the response
+        response = requests.post(url, headers=headers, data={"image": base64_data})
+
+        if response.status_code != 200:
+            return None
+
+        # Parse and return the image URL from Imgur response
+        result = response.json()
+        return result["data"]["link"]
+
+    except Exception as e:
+        print("Error uploading to Imgur:")
+        traceback.print_exc()
+        return None
+
+    finally:
+        os.remove(file)
+        
 
 @Client.on_message(filters.command(["pp", "reverse", "sauce"]))
 async def reverseImageSearch(_: Client, m: t.Message):
@@ -52,7 +85,7 @@ async def reverseImageSearch(_: Client, m: t.Message):
 async def ReverseResults(_: Client, query: t.CallbackQuery):
     data = query.data.split(".")
     userId = int(data[-1])
-    imageUrl = "https://graph.org" + data[-3] + "." + data[-2]
+    imageUrl = "https://i.imgur.com" + data[-3] + "." + data[-2]
     platform = "bing" if data[1] == "b" else "google" if data[1] == "g" else "yandex"
     if query.from_user.id != userId:
         return await query.answer("Not for you!", show_alert=True)
